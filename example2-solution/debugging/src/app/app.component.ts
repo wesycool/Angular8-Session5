c9096b26d8dce1869732358ec365ac8a0694f10d
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-10-29 21:40:16 -0400

    Added example 2

---
+++ example2-solution/debugging/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // servers;
+  servers: string[] = []
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    // const position = id + 1;
+    const position = id 
+    this.servers.splice(position, 1);
+  }
+}
